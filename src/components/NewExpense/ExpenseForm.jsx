f38164c322446be3015156341480c8f00c0fb1ae
motthy0 <sanscoding0@gmail.com>
2023-08-09 22:32:10 +0700

    p

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -88,7 +88,7 @@ const ExpenseForm = (props) => {
       </div>
       <div className='new-expense__actions'>
         <button type="button" onClick={props.onCancel}>Cancel</button>
-        <button type='submit'>Add Ex</button>
+        <button type='submit'>Add Expense</button>
       </div>
     </form>
   );
